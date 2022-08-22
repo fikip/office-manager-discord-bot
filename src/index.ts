96f5a70e34317ff0d063298835cd472631d92c3b
bozskejkaja <filipdrgon@gmail.com>
2022-08-22 15:44:47 +0200

    Added clearing of previous bot message to help with spam

--- src/index.ts
+++ src/index.ts
@@ -24,19 +24,15 @@ client.on('interactionCreate', async (interaction) => {
     if (rate && clientId) {
       const { data, error } = await supabase.from('users').upsert({ id: clientId, rate: rate })
       if (!error) {
-        console.log(data)
         await interaction.editReply({ content: `Your rate of ${rate} has been successfully recorded.` })
       }
     }
   } else if (interaction.commandName === COMMAND_NAMES.ADD_CHANNEL_FOR_CALCULATION) {
     await interaction.deferReply({ ephemeral: true })
     const channel = interaction.options.getChannel('channel')
-    console.log(channel)
-    // const clientId = interaction.user.id
     if (channel) {
       const { data, error } = await supabase.from('channels').upsert({ id: channel.id, name: channel.name })
       if (!error) {
-        console.log(data)
         await interaction.editReply({ content: `The channel ${channel.name} has been added to tracked channels.` })
       }
     }
@@ -54,14 +50,26 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
         if (channelMembers) {
           const usersInChannel = channelMembers.keys()
           const usersFromDB = await supabase.from('users').select('id, rate')
-          let finalRate = 0
           if (usersFromDB.data && !usersFromDB.error) {
+            let finalRate = 0
+            // Calculate hourly rate
             for (const userId of usersInChannel) {
               const foundUser = usersFromDB.data.find((user) => user.id === userId)
               if (foundUser) finalRate += foundUser.rate
             }
-            ;(client.channels.cache.get(newState.channel?.id) as TextChannel).send(
-              `Current hourly rate for this room is ${finalRate}€.`
+
+            // Clear last message in chat if it's from a bot, so that we're not spamming too much
+            const tailMessages = await (client.channels.cache.get(newState.channel?.id) as TextChannel).messages.fetch({
+              limit: 1,
+            })
+            const lastMessage = tailMessages.first()
+            if (lastMessage && lastMessage.author.bot) {
+              lastMessage.delete()
+            }
+
+            // Send the hourly rate message
+            const hourlyRateMessage = await (client.channels.cache.get(newState.channel?.id) as TextChannel).send(
+              `Current hourly rate for this room is ||${finalRate}||€.`
             )
           }
         }
